38810cc2eba03e4a639ea4d5419c1f290d35ce2e
FelipeEvangelistaNeves <felipeneves0303@gmail.com>
2026-05-19 15:16:57 -0300

    aaa

--- 12_03/1.py
+++
@@ -1,7 +0,0 @@
-premio=int(input("Digite o valor do prêmio: "))
-g1 = premio *0.47
-g2 = premio *0.34
-g3 = premio-g1-g2
-print("O ganhador do primeiro prêmio receberá: ",g1)
-print("O ganhador do segundo prêmio receberá: ",g2)
-print("O ganhador do terceiro prêmio receberá: ",g3)
--- 12_03/8.py
+++
@@ -1,3 +0,0 @@
-velocidade = int(input("Digite a velocidade em ms: "))
-velocidade_km_h = velocidade * 3.6
-print("A velocidade em km/h é:", velocidade_km_h)
